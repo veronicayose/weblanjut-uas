d6317401259f74ed7638c3b04860864a6235340e
Veronica Yose Ardilla <veronica.ardilla@student.matanauniversity.ac.id>
2021-12-17 03:40:40 +0700

    Add summary table

---
+++ LiteraBook/database/migrations/2021_12_15_205734_create_summary_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSummaryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('summary', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_readBook');
+            $table->integer('pages');
+            $table->longText('summary');
+            $table->integer('id_user')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('summary');
+    }
+}
