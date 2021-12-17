c4dbdbab52e7183aa996f4a9e606a3041241f2bf
Veronica Yose Ardilla <veronica.ardilla@student.matanauniversity.ac.id>
2021-12-17 20:58:34 +0700

    update

--- LiteraBook/resources/views/layouts/main.blade.php
+++ LiteraBook/resources/views/layouts/main.blade.php
@@ -1,16 +1,16 @@
 <x-app-layout>
     <div class="py-12">
-        {{-- <div class="row sm:px-6 lg:px-8">
-            <div class="col-4 bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="grid grid-cols-4 gap-4 sm:px-6 lg:px-8">
+            <div class="col-start-1 col-span-2 bg-white shadow-xl sm:rounded-lg">
                 @yield('content1')
             </div>
-            <div class="col-4 bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                @yield('content2')
+            <div class="col-end-4 col-span-2 bg-white shadow-xl sm:rounded-lg">
+                @yield('content1')
             </div>
-        </div> --}}
-        {{-- <div class="row sm:px-6 lg:px-8">
-            <x-jet-button class="border border-gray-300 text-black font-bold py-2 px-4 my-2 rounded">{{ __('Note') }}</x-jet-button>
-            <x-jet-button class="border border-gray-300 text-black font-bold py-2 px-4 my-2 rounded">{{ __('Quote') }}</x-jet-button>
-        </div> --}}
+        </div>
+        <div class="row sm:px-6 lg:px-8">
+            {{-- <x-jet-button class="border border-gray-300 text-black font-bold py-2 px-4 my-2 rounded">{{ __('Notes') }}</x-jet-button>
+            <x-jet-button class="border border-gray-300 text-black font-bold py-2 px-4 my-2 rounded">{{ __('Quote') }}</x-jet-button> --}}
+        </div>
     </div>
 </x-app-layout>
